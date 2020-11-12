088dfe550f8de8a8bce2228620eeac6725df0164
Salman Khaled Ovi <57897464+salkha@users.noreply.github.com>
2020-11-12 18:07:17 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+var randomNumber1 = Math.floor((Math.random() * 6) + 1);
+var imageSource1 = "images/dice" + randomNumber1 + ".png";
+var image1 = document.querySelectorAll("img")[0];
+image1.setAttribute("src", imageSource1);
+
+
+var randomNumber2 = Math.floor((Math.random() * 6) + 1);
+var imageSource2 = "images/dice" + randomNumber2 + ".png";
+var image2 = document.querySelectorAll("img")[1];
+image2.setAttribute("src", imageSource2);
+
+if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerHTML = "Player 1 wins";
+} else if (randomNumber1 < randomNumber2) {
+    document.querySelector("h1").innerHTML = "Player 2 wins";
+} else {
+    document.querySelector("h1").innerHTML = "Draw!!";
+}
+document.querySelector("button").addEventListener("click", function() {
+    console.log(this);
+    location.reload();
+    
+});
+
